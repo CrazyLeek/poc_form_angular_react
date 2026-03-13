b6fd7688c7e08f6f715e9a02770842631e8d01a0
CrazyLeek <ok@exemple>
2026-03-13 16:57:19 +0100

    feat: :rocket: Add flaky test

---
+++ react/cypress/e2e/form.cy.js
@@ -0,0 +1,9 @@
+describe("The Home Page", () => {
+  it("Submit the form without entering nothing", () => {
+    cy.visit("/");
+
+    cy.get('[data-cy="btn-submit"]').click();
+
+    cy.get('[data-cy="error-last-name"]'); // flaky test :)
+  });
+});
